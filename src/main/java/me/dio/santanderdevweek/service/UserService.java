351bac0a7c4a26b7ecc84caa03e639a6685aba83
Leandro Amaral <lramaral02m@gmail.com>
2023-08-24 17:52:14 -0300

    Implementando a Camada de Negócio

---
+++ src/main/java/me/dio/santanderdevweek/service/UserService.java
@@ -0,0 +1,8 @@
+package me.dio.santanderdevweek.service;
+
+import me.dio.santanderdevweek.domain.model.User;
+
+public interface UserService {
+    User findById(Long id);
+    User create(User userToCreate);
+}
